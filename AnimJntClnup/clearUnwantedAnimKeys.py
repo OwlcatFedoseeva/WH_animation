b805f6548536d79dfa4e299f9243adafc1dcda15
Valeriya Fedoseeva <136153985+ValeriyaFedoseeva@users.noreply.github.com>
2023-11-25 00:54:26 +0300

    updated the main script
    added more functionality to the script.
    Now you can choose the joints to clean up. Also you can choose what channels you can delete animation from. Lastly, you can choose the location where you want to export origin folder or the dummy folder.

--- AnimJntClnup/clearUnwantedAnimKeys.py
+++ AnimJntClnup/clearUnwantedAnimKeys.py
@@ -1,6 +1,7 @@
 import maya.cmds as cmds
 import os
 import maya.mel as mel
+import json
 from PySide2 import QtCore, QtGui, QtWidgets
 
 
@@ -13,7 +14,7 @@ files = []
 
 #PRESET = "C:/Users/Valeriya/Documents/maya/scripts/CHanimExport/UMA_Male_Rig_AnimationExport.fbxexportpreset"
 PRESET = USERAPPDIR + 'scripts/AnimJntClnup/UMA_Male_Rig_AnimationExport.fbxexportpreset'
-
+PATH = USERAPPDIR + 'scripts/AnimJntClnup/'
 
 class BatchCleanupUI(QtWidgets.QDialog):
     def __init__(self):
@@ -21,14 +22,17 @@ class BatchCleanupUI(QtWidgets.QDialog):
         self.anim_folder_path = None
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.setup_ui()
+        # Initialize json_path with a default value
+        initial_json_path = os.path.join(PATH, 'joints_set.json')  # Provide the correct JSON path
+        self.update_list_widget(initial_json_path)
 
     def setup_ui(self):
         # Set up the UI layout
         self.setWindowTitle("Batch jnt CleanUp")
         self.setObjectName("BatchjntCleanUpID")
-        self.setMinimumSize(400, 100)
-        self.setMaximumSize(800, 200)
-        self.resize(400, 100)
+        self.setMinimumSize(400, 250)
+        self.setMaximumSize(800, 500)
+        self.resize(400, 250)
 
         self.main_layout = QtWidgets.QVBoxLayout(self)
         self.main_layout.setAlignment(QtCore.Qt.AlignTop)
@@ -40,9 +44,55 @@ class BatchCleanupUI(QtWidgets.QDialog):
         self.main_layout.addWidget(self.name_label)
         self.save_name_field = QtWidgets.QLineEdit()
         self.main_layout.addWidget(self.save_name_field)
+
+        # Set Folder button
         self.btn_set_path = QtWidgets.QPushButton("Set Path")
         self.btn_set_path.clicked.connect(self.btn_set_path_clicked)
         self.main_layout.addWidget(self.btn_set_path)
+
+        # Set joints to clean up button
+        self.btn_set_selection = QtWidgets.QPushButton("Select Joint to clean up")
+        self.btn_set_selection.clicked.connect(self.save_joints_selection_list)
+        self.main_layout.addWidget(self.btn_set_selection)
+
+
+        self.listWidget = QtWidgets.QListWidget()
+        size_policy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        self.listWidget.setSizePolicy(size_policy)
+        self.main_layout.addWidget(self.listWidget)
+
+        self.chk_box_layout = QtWidgets.QHBoxLayout(self)
+        
+        # Check box
+        self.chk_box_trans = QtWidgets.QCheckBox("Translate")
+        self.chk_box_trans.setChecked(True)
+        self.chk_box_rot = QtWidgets.QCheckBox("Rotate")
+        self.chk_box_rot.setChecked(True)
+        self.chk_box_scl = QtWidgets.QCheckBox("Scale")
+        self.chk_box_scl.setChecked(True)
+
+        self.chk_box_layout.addWidget(self.chk_box_trans)
+        self.chk_box_layout.addWidget(self.chk_box_rot)
+        self.chk_box_layout.addWidget(self.chk_box_scl)
+
+        # Add the checkbox layout to the main layout
+        self.main_layout.addLayout(self.chk_box_layout)
+
+
+        self.export_option_layout = QtWidgets.QHBoxLayout(self)
+
+        # Save destination option
+        self.save_to_origin_folder_radio = QtWidgets.QRadioButton("Save to the folder of origin")
+        self.save_to_origin_folder_radio.setChecked(False)
+        self.save_to_origin_folder_radio.clicked.connect(self.save_option_changed)
+        self.export_option_layout.addWidget(self.save_to_origin_folder_radio)
+
+        self.save_to_animation_export_radio = QtWidgets.QRadioButton("Save to AnimationExport folder")
+        self.save_to_animation_export_radio.setChecked(True)
+        self.save_to_animation_export_radio.clicked.connect(self.save_option_changed)
+        self.export_option_layout.addWidget(self.save_to_animation_export_radio)
+
+        self.main_layout.addLayout(self.export_option_layout)
 
         # Batch Fix button
         self.btn_batch_process = QtWidgets.QPushButton("Batch Fix")
@@ -54,6 +104,7 @@ class BatchCleanupUI(QtWidgets.QDialog):
         folder_path = cmds.fileDialog2(dialogStyle=2, fileMode=3)
         if folder_path:
             self.save_name_field.setText(folder_path[0])
+        return None
 
     def new_scene_maya(self):
         # Create a new Maya scene
@@ -86,56 +137,108 @@ class BatchCleanupUI(QtWidgets.QDialog):
 
         return file_name
 
-    def jnt_clean_up(self):
-        # Clean up specified joints by cutting animation keys
-        jnt_list = [
-            'R_front_w____scale_01', 'R_front_weapon_slot_01', 'R_front_weapon_slot_01_ADJ', 'L_front_w____scale_05', 
-            'L_front_weapon_slot_05', 'L_front_weapon_slot_05_ADJ', 'L_front_w____scale_04', 'L_front_weapon_slot_04', 
-            'L_front_weapon_slot_04_ADJ', 'R_front_w____scale_02', 'R_front_weapon_slot_02', 'R_front_weapon_slot_02_ADJ', 
-            'C_back_w_____slot_11', 'C_back_weapon_slot_11', 'C_back_weapon_slot_11_ADJ', 'L_back_w_____slot_10', 
-            'L_back_weapon_slot_10', 'L_back_weapon_slot_10_ADJ', 'L_back_w_____slot_07', 'L_back_weapon_slot_07', 
-            'L_back_weapon_slot_07_ADJ', 'C_back_w_____slot_08', 'C_back_weapon_slot_08', 'C_back_weapon_slot_08_ADJ', 
-            'R_back_w_____slot_09', 'R_back_weapon_slot_09', 'R_back_weapon_slot_09_ADJ', 'R_back_w_____slot_06', 'R_back_weapon_slot_06', 'R_back_weapon_slot_06_ADJ'
-            ]
 
+    def set_selection(self):
+        jnt_list = []
+        sel = cmds.ls(sl=True, fl=True, type='joint')
+        
+        for s in sel:
+            jnt_list.append(s)
+        
+        print (jnt_list)	
+        return jnt_list
+
+    def save_joints_selection_list(self):
+        name = 'joints_set'
+        path = os.path.join(PATH, '%s.json' % name)
+        data = self.set_selection()
+        
+        with open(path, 'w') as f:
+            json.dump(data, f, indent=4)
+
+        # Update the list widget
+        self.update_list_widget(path)
+
+        # Use the saved data in the jnt_clean_up function
+        self.jnt_clean_up(path)
+
+
+    def update_list_widget(self, json_path):
+        self.listWidget.clear()
+        with open(json_path, 'r') as f:
+            data = json.load(f)
+
+        for item in data:
+            self.listWidget.addItem(item)
+
+    def jnt_clean_up(self, json_path):
+        # Clean up specified joints by cutting animation keys based on data in the JSON file
+        with open(json_path, 'r') as f:
+            jnt_list = json.load(f)
 
         for jnt in jnt_list:
             if cmds.objExists(jnt):
-                cmds.cutKey(jnt, attribute='translateX', option="keys")
-                cmds.cutKey(jnt, attribute='translateY', option="keys")
-                cmds.cutKey(jnt, attribute='translateZ', option="keys")
+                if self.chk_box_trans.isChecked():
+                    cmds.cutKey(jnt, attribute='translateX', option="keys")
+                    cmds.cutKey(jnt, attribute='translateY', option="keys")
+                    cmds.cutKey(jnt, attribute='translateZ', option="keys")
 
-                cmds.cutKey(jnt, attribute='rotateX', option="keys")
-                cmds.cutKey(jnt, attribute='rotateY', option="keys")
-                cmds.cutKey(jnt, attribute='rotateZ', option="keys")
+                if self.chk_box_rot.isChecked():
+                    cmds.cutKey(jnt, attribute='rotateX', option="keys")
+                    cmds.cutKey(jnt, attribute='rotateY', option="keys")
+                    cmds.cutKey(jnt, attribute='rotateZ', option="keys")
 
-                cmds.cutKey(jnt, attribute='scaleX', option="keys")
-                cmds.cutKey(jnt, attribute='scaleY', option="keys")
-                cmds.cutKey(jnt, attribute='scaleZ', option="keys")
+                if self.chk_box_scl.isChecked():
+                    cmds.cutKey(jnt, attribute='scaleX', option="keys")
+                    cmds.cutKey(jnt, attribute='scaleY', option="keys")
+                    cmds.cutKey(jnt, attribute='scaleZ', option="keys")
             else:
                 pass
 
     def create_directory(self, directory):
         # Create a given directory if it doesn't exist
-        if not os.path.exists(directory):
-            os.mkdir(directory)
+        try:
+            os.makedirs(directory)
+        except OSError as e:
+            if e.errno != os.errno.EEXIST:
+                raise
         return directory
+
+    def save_option_changed(self):
+        # Update the save option when the checkbox state changes
+        self.save_to_origin_folder = self.save_to_origin_folder_radio.isChecked()
+        self.save_to_animation_export = self.save_to_animation_export_radio.isChecked()
+
+
 
     def fbx_export(self):
         # Export FBX files
         global newDIRECTORY
-        dir = self.create_directory(directory=newDIRECTORY)
-        new_file_name = self.get_current_file_name().split('.')[0]
+        dir = self.save_name_field.text()  # Get the folder path from the UI
 
         if dir:
-            cmds.loadPlugin("fbxmaya")  # Load the fbxmaya plugin
-            cmds.select('ParentForExportDelete', hierarchy=True)
-            name_export = dir + '/' + new_file_name + '.fbx'
-            mel.eval('FBXLoadExportPresetFile -f "{}"'.format(PRESET))
-            mel.eval('FBXExport -s -f "{}" -force -options'.format(name_export))
-            print("FBX Exported:", name_export)
+            new_file_name = self.get_current_file_name().split('.')[0]  # Retrieve the current file name
+            if self.save_to_animation_export_radio.isChecked():
+                dir = self.create_directory(directory=newDIRECTORY)
+                cmds.loadPlugin("fbxmaya")  # Load the fbxmaya plugin
+                cmds.select('ParentForExportDelete', hierarchy=True)
+                name_export = dir + '/' + new_file_name + '.fbx'
+                mel.eval('FBXLoadExportPresetFile -f "{}"'.format(PRESET))
+                mel.eval('FBXExport -s -f "{}" -force -options'.format(name_export))
+                print("FBX Exported:", name_export)
+
+            if self.save_to_origin_folder_radio.isChecked():
+                # dir = self.btn_set_path_clicked()  # This line is not necessary
+                cmds.loadPlugin("fbxmaya")  # Load the fbxmaya plugin
+                cmds.select('ParentForExportDelete', hierarchy=True)
+                name_export = dir + '/' + new_file_name + '.fbx'
+                mel.eval('FBXLoadExportPresetFile -f "{}"'.format(PRESET))
+                mel.eval('FBXExport -s -f "{}" -force -options'.format(name_export))
+                print("FBX Exported:", name_export)
+
         else:
             print("Error creating directory.")
+
 
     def process(self):
         # Main process function
@@ -146,8 +249,14 @@ class BatchCleanupUI(QtWidgets.QDialog):
 
             for f in files:
                 cmds.file(f, open=True, type='FBX', force=True)
-                self.jnt_clean_up()
+                json_path = os.path.join(PATH, 'joints_set.json')  # Provide the correct JSON path
+                self.jnt_clean_up(json_path)
                 self.fbx_export()
+
+            # Clear the files list for the next iteration
+            cmds.confirmDialog(title='Process Complete', message='Batch Process is Done', button=['OK'])
+            del files[:]
+
 
 
 def clean_ui():
