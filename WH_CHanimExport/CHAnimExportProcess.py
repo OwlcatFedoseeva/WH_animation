5bd0c0041c2971ba03ca026c0f57ed3509199434
Valeriya Fedoseeva <151736850+OwlcatFedoseeva@users.noreply.github.com>
2024-04-03 16:42:50 +0300

    Update CHAnimExportProcess.py
    clean up joints have been updated.
    Previously the list was in the json file but some people were having troubles using this code this way? so I had to implement the list within main body of the script

--- WH_CHanimExport/CHAnimExportProcess.py
+++ WH_CHanimExport/CHAnimExportProcess.py
@@ -21,13 +21,17 @@ class ch_anim_export(QtWidgets.QDialog):
         #self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
         self.setup_ui()
         self.toDel = None
+        self.position = 'Position'
+        self.root = 'Root_M'
+        self.folder_path=None
+
 
     def setup_ui(self):
-        self.setWindowTitle("Character Animation Export 3.2")
+        self.setWindowTitle("Character Animation Export 4.1")
         self.setObjectName("CHAnimExportID")
-        self.setMinimumSize(400, 200)
-        self.setMaximumSize(800, 40)
-        self.resize(400, 200)
+        self.setMinimumSize(400, 315)
+        self.setMaximumSize(800, 325)
+        self.resize(400, 315)
         
         self.main_layout = QtWidgets.QVBoxLayout()
         self.main_layout.setAlignment(QtCore.Qt.AlignTop)
@@ -103,7 +107,27 @@ class ch_anim_export(QtWidgets.QDialog):
         divider_line.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.main_layout.addWidget(divider_line)
 
+        self.export_creature_rig_btn()
         self.export_creature_anim_btn()
+
+
+        # Animation Folder UI components
+        self.name_label = QtWidgets.QLabel('Animation Folder:')
+        self.main_layout.addWidget(self.name_label)
+
+        self.btn_layout_B = QtWidgets.QHBoxLayout()
+        self.main_layout.addLayout(self.btn_layout_B)
+
+
+        self.batch_path_name_field = QtWidgets.QLineEdit()
+        self.batch_path_name_field.setPlaceholderText("Select folder with maya files for batch export") 
+        self.btn_layout_B.addWidget(self.batch_path_name_field)
+        # Set Folder button
+        self.batch_btn_set_path = QtWidgets.QPushButton("Set Path")
+        self.batch_btn_set_path.clicked.connect(self.batch_btn_set_path_clicked)
+        self.btn_layout_B.addWidget(self.batch_btn_set_path)
+
+        self.batch_export_creature_anim_btn()
 
     def createDirectory(self, directory=DIRECTORY):
         """
@@ -116,7 +140,6 @@ class ch_anim_export(QtWidgets.QDialog):
         if not os.path.exists(directory):
             os.mkdir(directory)
         return directory
-
 
     def update_file_name(self):
         global newFileName
@@ -133,7 +156,6 @@ class ch_anim_export(QtWidgets.QDialog):
         else:
             print("No folder selected.")
         return None
-
 
     def create_comboboxA(self):
         self.combobox_race = QtWidgets.QComboBox()
@@ -162,6 +184,26 @@ class ch_anim_export(QtWidgets.QDialog):
         self.btn_bake_creature_anim.clicked.connect(self.creature_export_process)
         self.btn_bake_creature_anim.setToolTip("Click to export creature animation")
         self.main_layout.addWidget(self.btn_bake_creature_anim)
+
+    def batch_export_creature_anim_btn(self):
+        '''
+        In this function we create button that will bake animation on Creatures.
+        '''
+        # Buttons
+        self.btn_batch_bake_creature_anim = QtWidgets.QPushButton("Batch Creature Animation Export")
+        self.btn_batch_bake_creature_anim.clicked.connect(self.batch_creature_export_process)
+        self.btn_batch_bake_creature_anim.setToolTip("Click to export creature animation")
+        self.main_layout.addWidget(self.btn_batch_bake_creature_anim)
+
+    def export_creature_rig_btn(self):
+        '''
+        In this function we create button that will export creature RIG (skeleton with all meshes that are skinned to it) as FBX file into work folder.
+        '''
+
+        self.btn_export_creature_rig = QtWidgets.QPushButton("Export RIG as FBX")
+        self.btn_export_creature_rig.clicked.connect(self.creature_rig_export_process)
+        self.btn_export_creature_rig.setToolTip("Click to export creature RIG file as FBX into your work folder")
+        self.main_layout.addWidget(self.btn_export_creature_rig)
 
     def imprtRef(self):
         '''
@@ -224,8 +266,8 @@ class ch_anim_export(QtWidgets.QDialog):
 
         if selected_option == "Human":
             print("Clean up for for Humans and Eldar")
-            list = ['Spine_1', 'Spine_2', 'Spine_3', 'Neck', 'Head_ADJ', 'Head_Helmet_ADJ',
-            'Skull_Scale', 'Face_Scale', 'L_eyebrow', 'R_eyebrow', 'R_eyelish_top', 'R_eyelish_top_end',
+            list = ['Spine_1', 'Spine_2', 'Spine_3', 'Neck', 'Head_ADJ', 'Head_Helmet_ADJ', 'Head_helmet_ADJ',
+            'Skull_Scale', 'Face_Scale',  'R_eyelish_top', 'R_eyelish_top_end',
             'L_eyelish_top', 'L_eyelish_top_end', 'R_eyelish_bottom', 'R_eyelish_bottom_end', 'L_eyelish_bottom',
             'L_eyelish_bottom_end', 'R_eye', 'R_eye_end', 'L_eye', 'L_eye_end', 'Jaw_Scale', 'Neck_ADJ', 'L_Clavicle',
             'L_Up_arm', 'L_ForeArm', 'L_Hand', 'L_Toe_1_01', 'L_Toe_1_02', 'L_Toe_1_02_ADJ', 'L_Toe_1_01_ADJ',
@@ -241,18 +283,20 @@ class ch_anim_export(QtWidgets.QDialog):
             'R_Toe_3_01', 'R_Toe_3_02', 'R_Toe_3_02_ADJ', 'R_Toe_3_01_ADJ', 'R_Hand_ADJ',
             'R_ForeArm_ADJ', 'R_ForeArm_Twist_ADJ', 'R_Up_Arm_Muscl_ADJ', 'R_Up_arm_ADJ', 'R_Clavicle_ADJ',
             'Spine_2_ADJ', 'C_back_w_____slot_11', 'C_back_weapon_slot_11', 'C_back_weapon_slot_11_ADJ', 'Stomach',
-            'Stomach_ADJ', 'Spine_1_ADJ'] 
-            #['L_Up_leg', 'R_Pre_Up_Leg']
+            'Stomach_ADJ', 'Spine_1_ADJ', 'R_leg', 'R_foot', 'R_toe',  'L_leg', 'L_foot', 'L_toe', 'L_Up_leg', 'R_Pre_Up_Leg']
 
             for i in list:
-                cmds.select(i)
-                cmds.cutKey(at='translate')
+                if cmds.objExists(i):
+                    cmds.select(i)
+                    cmds.cutKey(at='translate')
+                else:
+                    print(i + 'is not in the scene. Skipping...')
 
         elif selected_option == "SpaceMarine":
             print("Clean up for Spacemarine")
 
-            list = ['Spine_1', 'Spine_2', 'Spine_3', 'Neck', 'Head_ADJ', 'Head_Helmet_ADJ',
-            'Skull_Scale', 'Face_Scale', 'L_eyebrow', 'R_eyebrow', 'R_eyelish_top', 'R_eyelish_top_end',
+            list = ['Spine_1', 'Spine_2', 'Spine_3', 'Neck', 'Head_ADJ', 'Head_Helmet_ADJ', 'Head_helmet_ADJ',
+            'Skull_Scale', 'Face_Scale', 'R_eyelish_top', 'R_eyelish_top_end',
             'L_eyelish_top', 'L_eyelish_top_end', 'R_eyelish_bottom', 'R_eyelish_bottom_end', 'L_eyelish_bottom',
             'L_eyelish_bottom_end', 'R_eye', 'R_eye_end', 'L_eye', 'L_eye_end', 'Jaw_Scale', 'Neck_ADJ', 'L_Clavicle',
             'L_Up_arm', 'L_ForeArm', 'L_Hand', 'L_Toe_1_01', 'L_Toe_1_02', 'L_Toe_1_02_ADJ', 'L_Toe_1_01_ADJ',
@@ -271,8 +315,11 @@ class ch_anim_export(QtWidgets.QDialog):
             'Stomach_ADJ', 'Spine_1_ADJ']
 
             for i in list:
-                cmds.select(i)
-                cmds.cutKey(at='translate')
+                if cmds.objExists(i):
+                    cmds.select(i)
+                    cmds.cutKey(at='translate')
+                else:
+                    print(i + 'is not in the scene. Skipping...')
 
         print("Skeleton clean up is complete")
 
@@ -361,7 +408,6 @@ class ch_anim_export(QtWidgets.QDialog):
         messageBox.setIcon(QtWidgets.QMessageBox.Information)
         messageBox.exec_()
 
-
     def runProcess(self):
         '''
         This function runs above functions and initiates the export process for Human| Eldar| SpaceMarine characters.
@@ -448,8 +494,7 @@ class ch_anim_export(QtWidgets.QDialog):
         # After exporting the FBX file, show the dialog with the export location
         self.showExportLocationDialog()
 
-        
-    
+            
     # CREATURE EXPORT part of the scrript
 
     def bakeProcess(self):
@@ -542,6 +587,225 @@ class ch_anim_export(QtWidgets.QDialog):
         self.creature_anim_export()
         self.showExportLocationDialog()
         return
+    
+
+    def show_error_dialog(self):
+        '''
+        Shows a dialog box with error and way to solve it.
+        '''
+        
+        messageBoxError = QtWidgets.QMessageBox()
+        messageBoxError.setWindowTitle("RIG Export ERROR")
+        messageBoxError.setText("This RIG doesn't contain Position joint. Either create it manually or rename existing joint at the origin to Position and then continue.")
+        messageBoxError.setIcon(QtWidgets.QMessageBox.Information)
+        messageBoxError.exec_()
+
+    def disconnectAll(self, node, source=True, destination=True):
+        connectionPairs = []
+        if source:
+            conns = cmds.listConnections(node, plugs=True, connections=True, destination=False)
+            if conns:
+                connectionPairs.extend(zip(conns[1::2], conns[::2]))
+        
+        if destination:
+            conns = cmds.listConnections(node, plugs=True, connections=True, source=False)
+            if conns:
+                connectionPairs.extend(zip(conns[::2], conns[1::2]))
+        
+        for srcAttr, destAttr in connectionPairs:
+            cmds.disconnectAttr(srcAttr, destAttr)
+
+    def creature_rig_export_checker(self):
+        
+        if cmds.objExists(self.position):
+            self.position = 'Position'
+        else:
+            if cmds.objExists(self.root):
+                position = cmds.jnt(n='Position')
+                cmds.parent(self.root, self.position)
+            else:
+                self.show_error_dialog()
+           
+    def creature_rig_scene_cleanup(self):
+        self.creature_rig_export_checker()
+            
+        if cmds.objExists('ConstraintSystem'):
+            cmds.delete('ConstraintSystem')
+        else:
+            pass
+		
+        blendC = cmds.ls(type='blendColors')	
+        if blendC:
+            print('BlindDataTemplate nodes found. Deleting...')
+            cmds.delete(blendC)
+
+
+        # Get a list of selected objects
+        selected_objects = cmds.ls('Position', dag=True)
+
+        for obj in selected_objects:
+            # Check if there is a connection to the visibility attribute
+            visibility_connections = cmds.listConnections(obj + '.visibility', source=True, destination=False)
+            if visibility_connections:
+                # Check if 'Main.jointVis' is among the connections
+                if 'Main.jointVis' in visibility_connections:
+                    # Disconnect 'Main.jointVis' from the visibility attribute
+                    cmds.disconnectAttr('Main.jointVis', obj + '.visibility')
+                    print("Disconnected 'Main.jointVis' from", obj + ".visibility")
+                else:
+                    print("No connection to 'Main.jointVis' found for", obj + ".visibility")
+            else:
+                print("No connections found for", obj + ".visibility")
+
+        # Iterate over each selected object
+        for obj in selected_objects:
+            attr_names = ['sx', 'sy', 'sz']
+            for at in attr_names:
+                scale_attribute = obj + "." + at
+                print(scale_attribute)
+                
+                # List connections for the scale attribute
+                connections = cmds.listConnections(scale_attribute, d=False, s=True)
+                if connections:
+                    for connection in connections:
+                        # Disconnect each connection
+                        cmds.disconnectAttr(connection + '.' + at, scale_attribute)
+                else:
+                    print("No connections found for", scale_attribute)
+                
+                if cmds.listRelatives(self.position, parent=True):
+                    cmds.parent(self.position, world=True)
+                else:
+                    print("Object, '{}', is already a child of the parent, 'world'.".format(self.position))
+
+                if cmds.objExists('Group'):    
+                    cmds.delete('Group')
+                else:
+                    print("Group node doesn't exist. If RIG group exists name it Group and run this script again.")
+
+    def creature_rig_export(self):
+            '''
+            This function for export of Creatures as FBX file into a designated location in UI.
+            '''
+            cmds.loadPlugin("fbxmaya")
+            mel.FBXLoadExportPresetFile(f=presetPathName)
+
+            # Check if any display layers exist and delete them
+            display_layers = cmds.ls(type='displayLayer')
+            if display_layers:
+                cmds.delete(display_layers)
+
+
+            all_objects = cmds.ls()
+
+            # Create an empty list to store polygon objects
+            polygon_objects = []
+
+            # Iterate through all objects and check if they are polygons
+            for obj in all_objects:
+                if cmds.objectType(obj) == 'transform':
+                    # Check if the object has polygon data
+                    shapes = cmds.listRelatives(obj, shapes=True)
+                    if shapes:
+                        for shape in shapes:
+                            if cmds.nodeType(shape) == 'mesh':
+                                polygon_objects.append(obj)
+                                break
+
+            # Select all polygon objects
+            cmds.select(polygon_objects, replace=True)
+            cmds.select(self.position, hi=True, add=True)
+
+
+            # Get the folder path from the save_name_field
+            self.folder_path = self.save_name_field.text()
+
+            # Call update_file_name to ensure newFileName is updated
+            self.update_file_name()
+
+
+            if cmds.objExists('Group'):
+                cmds.delete('Group')
+            else:
+                pass
+ 
+
+            if self.rbtnA.isChecked():
+                print("Folder path is not set. Creating a default folder.")
+                self.folder_path = self.createDirectory(DummyFolder)
+                nameExport = self.folder_path + '/' + newFileName + '.fbx'
+                print("Exporting FBX to:", nameExport)  # Add this line to verify the file path
+                mel.FBXExport(s=True, f=nameExport, force=True, options=True)
+                return
+
+            elif self.rbtnB.isChecked():
+                nameExport = self.folder_path + '/' + newFileName + '.fbx'
+                mel.FBXExport(s=True, f=nameExport, force=True, options=True)
+                print("FBX file exported to: {}".format(nameExport))
+                return
+            
+            print("FBX file exported to: {}".format(nameExport))
+            return
+
+    def creature_rig_export_process(self):
+        self.creature_rig_scene_cleanup()
+        self.creature_rig_export()
+
+
+    def new_scene_maya(self):
+        # Create a new Maya scene
+        cmds.file(new=True, force=True)
+
+    def batch_btn_set_path_clicked(self):
+        # Open file dialog to set the animation folder path
+        anim_folder_path = cmds.fileDialog2(dialogStyle=2, fileMode=3)
+        if anim_folder_path:
+            selected_folder_path = anim_folder_path[0]
+            self.batch_path_name_field.setText(selected_folder_path)
+            self.anim_folder_path = selected_folder_path  # Set the attribute here
+            self.folder_path = selected_folder_path  # Update folder_path here
+            return selected_folder_path
+
+
+    def batch_creature_export_process(self):
+        # Get the folder path from the UI
+        if not self.folder_path:
+            print("Folder path is not set. Creating a default folder.")
+            self.folder_path = self.createDirectory(DummyFolder)
+
+        # Get a list of creatures or rigs to export
+        path = self.anim_folder_path
+        files = [os.path.join(path, f) for f in os.listdir(path) if f.lower().endswith(".ma")]
+
+        for creature in files:
+            # Open or create a new scene in Maya
+            self.new_scene_maya()
+
+            # Import the creature or rig
+            cmds.file(creature, i=True)
+
+            # Remove namespaces
+            self.remove_namespaces()
+
+            # Run the export process for the creature or rig
+            self.creature_rig_export_process()
+
+            # Construct the export file name
+            export_file_name = creature + '.fbx'
+            export_file_path = os.path.join(self.folder_path, export_file_name)
+
+            # Export the creature or rig as FBX
+            mel.FBXExport(s=True, f=export_file_path, force=True, options=True)
+
+            print("Exported {} to {}".format(creature, export_file_path))
+
+        print("Batch export completed.")
+
+
+
+
+
+# Call the function to run the process
 
 def clean_ui():
     if cmds.window("AnimationTransferID", exists=True):
